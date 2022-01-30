25611fd28ef45d703b2f3f13e59a4d99b19b2d5d
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-01-30 15:31:41 +0100

    feat: language detection

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -2,13 +2,14 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { pl } from "./pl";
 import { en } from "./en";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
     resources: { pl, en },
     supportedLngs: ["en", "pl"],
-    lng: "en",
 
     interpolation: {
       escapeValue: false,
