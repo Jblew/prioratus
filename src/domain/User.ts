b6eb488a08e0e27f2d83292927a56d8d2ab929ce
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-02-06 20:36:44 +0100

    feat: domain user

--- src/domain/User.ts
+++ src/domain/User.ts
@@ -3,7 +3,8 @@ import { Named } from "@/utils";
 export interface User {
   ID: UserID;
   displayName: string;
+  email: string;
+  created: Date;
 }
 
-export type UserID = Named<string, User>
-
+export type UserID = Named<string, User>;
