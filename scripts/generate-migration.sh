f8fe4f7ec5ad70fb38c92b714de9b20ab5021da7
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-02-06 21:42:51 +0100

    fix: valid docker volume for migration generation

--- scripts/generate-migration.sh
+++ scripts/generate-migration.sh
@@ -1,3 +1,6 @@
-# You have to specify MIGRATION_NAME= env
-docker-compose -f docker-compose.yml -f docker-compose.cli.yml build generate_migration && \
-    docker-compose -f docker-compose.yml -f docker-compose.cli.yml up generate_migration
+#/usr/bin/env bash
+export MIGRATION_NAME=$1 
+docker-compose \
+    -f docker-compose.yml -f docker-compose.cli.yml build generate_migration && \
+docker-compose \
+    -f docker-compose.yml -f docker-compose.cli.yml up generate_migration
