7ce2024ed13d34cc6e2ab91ba3dc857eaf89bf4c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-02-06 21:47:59 +0100

    feat: message domain definition

--- src/domain/Messaging.ts
+++ src/domain/Messaging.ts
@@ -1,7 +1,20 @@
 import { Named } from "@/utils";
+import { Account } from "./Account";
 
 export interface MessagingProvider {
   ID: MessagingProviderID;
 }
 
 export type MessagingProviderID = Named<string, "messagingProviderID">;
+
+export interface Message {
+  ID: MessageID;
+  time: Date;
+  text: string;
+  providerID: MessagingProviderID;
+  account: Account;
+  direction: "user" | "system";
+  responseTo?: Message;
+}
+
+export type MessageID = Named<string, "messageID">;
