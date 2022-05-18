6d48639e25a48d5262c833e7118ad3bd176eb6be
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-05-18 16:21:23 +0200

    feat: use prisma for database management

---
+++ backend/prisma/migrations/20220518141641_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,14 @@ import express from "express";
 import { getAuthMiddleware } from "@/auth";
 import { getRoutes } from "./router";
 import morgan from "morgan";
-import { initDatabase } from "./db";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 async function run() {
   const app = express();
   const port = process.env.PORT || 3000;
   const routeBase = process.env.ROUTE_BASE || "/";
-
-  await initDatabase();
 
   app.use(
     morgan(":method :url :status :res[content-length] - :response-time ms")
@@ -25,4 +25,6 @@ async function run() {
 run().catch((err) => {
   console.error("Error in main loop, exitting", err);
   process.exit(1);
+}).finally(async () => {
+  await prisma.$disconnect()
 });
