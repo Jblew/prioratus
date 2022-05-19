7caf2298618260f8f63d22149370326038eb1663
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-05-19 12:07:53 +0200

    feat: check if user is enrolled in beta

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
-import express from "express";
-import { getAuthMiddleware } from "@/auth";
-import { getRoutes } from "./router";
-import morgan from "morgan";
+import express from "express"
+import { getAuthMiddleware } from "@/auth"
+import { getRoutes } from "./router"
+import morgan from "morgan"
 import { PrismaClient } from '@prisma/client'
+import { loadUserConfigs } from "./userconfigs"
 
 const prisma = new PrismaClient()
+const userConfigs = loadUserConfigs()
+console.log("User configs:", JSON.stringify(userConfigs, undefined, 2))
 
 async function run() {
-  const app = express();
-  const port = process.env.PORT || 3000;
-  const routeBase = process.env.ROUTE_BASE || "/";
+  const app = express()
+  const port = process.env.PORT || 3000
+  const routeBase = process.env.ROUTE_BASE || "/"
 
   await prisma.notification.create({
     data: {
@@ -21,18 +24,18 @@ async function run() {
 
   app.use(
     morgan(":method :url :status :res[content-length] - :response-time ms")
-  );
-  app.use(routeBase, getAuthMiddleware());
-  app.use(routeBase, getRoutes());
+  )
+  app.use(routeBase, getAuthMiddleware())
+  app.use(routeBase, getRoutes({ userConfigs }))
 
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
+    console.log(`Example app listening on port ${port}`)
+  })
 }
 
 run().catch((err) => {
-  console.error("Error in main loop, exitting", err);
-  process.exit(1);
+  console.error("Error in main loop, exitting", err)
+  process.exit(1)
 }).finally(async () => {
   await prisma.$disconnect()
-});
+})
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import { authOr403 } from "@/auth"
 import { envMust } from "./utils"
+import { UserConfig } from "./userconfigs"
 
 const frontendRecirectURL = envMust("FRONTEND_RECIRECT_URL")
-export function getRoutes() {
+export function getRoutes({ userConfigs }: { userConfigs: UserConfig[] }) {
   const router = express.Router()
   router.get("/", (_req, res) => {
     res.redirect(frontendRecirectURL)
@@ -14,11 +15,18 @@ export function getRoutes() {
   })
 
   router.get("/profile", authOr403(), (req, res) => {
+    const user = req.oidc.user!
     const profile = {
-      ...req.oidc.user,
-      enrolledInBeta: true,
+      ...user,
+      enrolledInBeta: isUserEnrolledInBeta(user, userConfigs),
     }
     res.send(JSON.stringify(profile))
   })
   return router
+}
+
+function isUserEnrolledInBeta(user: Record<string, unknown>, userConfigs: UserConfig[]): boolean {
+  const email = user.email
+  if (!email) { return false }
+  return userConfigs.find(conf => conf.user === email) !== undefined
 }
